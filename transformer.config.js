1bb0adc47fbd88ee2bafd130fbaab797cc02926f
도시다람쥐 <86960007+Citysquirrel@users.noreply.github.com>
2022-07-13 13:09:06 +0900

    Update transformer.config.js

--- transformer.config.js
+++ transformer.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   typeConfig: {
     color: true,
-    fontSizes: true,
-    lineHeights: true,
+    fontSizes: false,
+    lineHeights: false,
     boxShadow: true,
     borderWidth: false,
   },
 };
-//? 코드 특성상 굳이 *.config.js 파일로 나눌 필요는 없을 듯? 나누는 게 더 좋을 것 같기도?
